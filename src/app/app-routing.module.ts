8777f6ed679ca3a766a28cdb58483bf3b3270003
Rumi Jahan Dalas <rumi@bd.batworld.com>
2023-09-14 13:06:55 +0600

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'bank-acc-info/local-acc',
+    pathMatch:'full'
+  },
+  {
+    path:'',
+    loadChildren: () => import('./bank-account/bank-account.module').then(m=>m.BankAccountModule)
+  },
+  {
+    path:'**',
+    component:PageNotFoundComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
